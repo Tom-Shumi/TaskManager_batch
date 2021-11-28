cad575bd1d7e81d70d2c15beec2a7b95ac020fec
Tom-Shumi <tshumiya281@gmail.com>
2021-11-28 19:17:56 +0900

    add batch infra DONE

--- src/main/kotlin/com/ne/jp/shumipro_batch/config/BatchConfig.kt
+++ src/main/kotlin/com/ne/jp/shumipro_batch/config/BatchConfig.kt
@@ -1,6 +1,5 @@
 package com.ne.jp.shumipro_batch.config
 
-import com.ne.jp.shumipro_batch.tasklet.DummyTasklet
 import com.ne.jp.shumipro_batch.tasklet.ElasticsearchMigrationTasklet
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
@@ -10,16 +9,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.context.annotation.Bean
-import org.springframework.batch.core.launch.support.SimpleJobLauncher
-
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
-
-import org.springframework.batch.core.repository.JobRepository
-
-import org.springframework.batch.core.launch.JobLauncher
-
-
-
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 
 
 @EnableBatchProcessing
@@ -27,48 +17,21 @@ import org.springframework.batch.core.launch.JobLauncher
 class BatchConfig(
     val jobBuilderFactory: JobBuilderFactory,
     val stepBuilderFactory: StepBuilderFactory,
-    val elasticsearchMigrationTasklet: ElasticsearchMigrationTasklet,
-    val dummyTasklet: DummyTasklet,
+    val elasticsearchMigrationTasklet: ElasticsearchMigrationTasklet
 ) {
 
     @Bean
-    fun helloWorldJob1 (helloWorldStep1: Step): Job {
-        return jobBuilderFactory["helloWorldJob1"]
-            .incrementer(Incrementer())
-            .flow(helloWorldStep1)
-            .end()
+    fun elasticsearchMigrationJob (): Job {
+        return jobBuilderFactory
+            .get("elasticsearchMigrationJob")
+            .incrementer(RunIdIncrementer())
+            .start(elasticsearchMigrationStep())
             .build()
     }
-    @Bean
-    fun helloWorldStep1 (): Step {
-        return stepBuilderFactory["helloWorldStep1"]
+
+    fun elasticsearchMigrationStep (): Step {
+        return stepBuilderFactory.get("elasticsearchMigrationStep")
             .tasklet(elasticsearchMigrationTasklet)
             .build()
-    }
-
-    @Bean
-    fun helloWorldJob2(helloWorldStep2: Step): Job {
-        return jobBuilderFactory["helloWorldJob2"]
-            .incrementer(Incrementer())
-            .flow(helloWorldStep2)
-            .end()
-            .build()
-    }
-    @Bean
-    fun helloWorldStep2(): Step {
-        return stepBuilderFactory["helloWorldStep2"]
-            .tasklet(dummyTasklet)
-            .build()
-    }
-
-    @Bean
-    fun jobLauncher(jobRepository: JobRepository): JobLauncher {
-        val taskExecutor = ThreadPoolTaskExecutor()
-        taskExecutor.initialize()
-        val jobLauncher = SimpleJobLauncher()
-        jobLauncher.setJobRepository(jobRepository)
-        jobLauncher.setTaskExecutor(taskExecutor)
-
-        return jobLauncher
     }
 }
